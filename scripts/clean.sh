1fea678010dfe23f2b3c3c806e592527b78c96c8
Marketionist <marketionist@gmail.com>
2017-03-14 21:59:58 +0200

    Updated clean.sh with clear description

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-npm cache clean && rm -rf node_modules/ .cache/ && echo " -> npm cache cleaned, node_modules/ .cache/ removed"
+npm cache clean && rm -rf node_modules/ .cache/ && echo " -> npm cache cleaned, node_modules/ and .cache/ folders removed"
