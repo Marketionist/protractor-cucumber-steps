a7e453025dcf2446eff6831d3d251e44be4dcbf7
Marketionist <marketionist@gmail.com>
2017-04-16 23:28:00 +0300

    Updated page objects

--- tests/page_objects/test2.page.js
+++ tests/page_objects/test2.page.js
@@ -4,7 +4,9 @@ module.exports = (function () {
 
     let test2Page = {
 
-        mainURL: 'http://www.google.com/ncr'
+        pageTest2: 'http://localhost:8001/test2.html',
+        textGreen: 'Green',
+        dropdownColors: '#dropdown-colors'
 
     };
 
