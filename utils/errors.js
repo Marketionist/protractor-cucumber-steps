7621a81228627603699a89317bc6609470d66ad9
Marketionist <marketionist@gmail.com>
2017-05-17 00:54:56 +0300

    Updated errors.js

--- utils/errors.js
+++ utils/errors.js
@@ -2,9 +2,5 @@
 
 module.exports = {
     PRESENT: 'Element should be present, but it is not',
-    CLICKABLE: 'should be clickable, but it is not',
-    NAVIGATION: {
-        BASE_URL: 'Base url should be a valid absolute url.',
-        ROOT: 'Please provide "driver.baseUrl" or set BASE_URL in your env to use root function.'
-    }
+    CLICKABLE: 'should be clickable, but it is not'
 };
