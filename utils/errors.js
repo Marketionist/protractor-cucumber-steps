a22d6c0403e75067547e1a3d2c58a408d4728b8d
Marketionist <marketionist@gmail.com>
2017-05-17 00:51:52 +0300

    Added errors.js for error texts handling

---
+++ utils/errors.js
@@ -0,0 +1,10 @@
+'use strict';
+
+module.exports = {
+    PRESENT: 'Element should be present, but it is not',
+    CLICKABLE: 'should be clickable, but it is not',
+    NAVIGATION: {
+        BASE_URL: 'Base url should be a valid absolute url.',
+        ROOT: 'Please provide "driver.baseUrl" or set BASE_URL in your env to use root function.'
+    }
+};
