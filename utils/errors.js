9f99fb12aa1542a2d3c42dc2bedcad3e73fb41ce
Marketionist <marketionist@gmail.com>
2017-07-27 08:50:39 +0300

    Added errors.ALERT_PRESENT

--- utils/errors.js
+++ utils/errors.js
@@ -2,6 +2,7 @@
 
 module.exports = {
     ELEMENT_PRESENT: 'Element should be present, but it is not',
+    ALERT_PRESENT: 'Browser alert should be present, but it is not',
     CLICKABLE: 'should be clickable, but it is not',
     REGEXP: 'does not match provided regexp',
     CONTAIN: 'does not contain'
